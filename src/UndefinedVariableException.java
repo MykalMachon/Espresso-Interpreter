4a45fd0f5b97ab2bb70093153964216b830ceeec
Mykal Machon <mykalmachon@gmail.com>
2019-03-11 23:13:56 -0700

    Add UndefinedVariableException.java
    + Add a custom exception for the variable class that will be thrown when a variable is not properly initialized or is otherwise undefined.

---
+++ src/UndefinedVariableException.java
@@ -0,0 +1,7 @@
+public class UndefinedVariableException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public UndefinedVariableException(String err) {
+    super(err);
+  }
+}
