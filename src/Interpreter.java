e42623164b3d0827eb9be0040c833ccbe32db85f
Mykal Machon <mykalmachon@gmail.com>
2019-03-13 22:17:17 -0700

    Adding Comments to Interpreter methods
    + Added comments to the Interpreter constructor
    + Added comments to the init method
    + Added comments to the parseLine method
    + Added comments to the getInput method

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -4,17 +4,26 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Interpreter {
-  private int currLineIndex;
-  private int currTokenIndex;
   private LinkedList<String> lines;
   private Variable[] variableTable = new Variable[122];
 
+  /**
+   * * Interpreter
+   * 
+   * This is a constructor for an instance of interpreter. This method does the
+   * following 1. Takes the passed in espFile and breaks it into an String array
+   * of lines 2. Creates a 124 character variableTable array to store variables
+   * from a-z && A-Z
+   * 
+   * @param espFile A File that references the file passed in from Espresso.java
+   *                (the user interface)
+   * @throws FileNotFoundException when espFile is not found
+   * @return void
+   */
   public Interpreter(File espFile) throws FileNotFoundException {
     // Reads in the file and initializes this.lines;
     this.lines = new LinkedList<String>();
-    this.currLineIndex = 0;
-    this.currTokenIndex = 0;
-    for(int i = 0; i < 'z'; i++){
+    for (int i = 0; i < 'z'; i++) {
       this.variableTable[i] = new Variable();
     }
     try {
@@ -29,6 +38,15 @@ public class Interpreter {
     }
   }
 
+  /**
+   * * init
+   * 
+   * This intializes the interpreter instance. It loops through each line, breaks
+   * the line into "tokens" by splitting them on " " and then passes the lines
+   * tokens into parseLine() as an array of strings
+   * 
+   * @return void
+   */
   public void init() {
     for (int i = 0; i < this.lines.size(); i++) {
       String currLine = this.lines.get(i);
@@ -37,11 +55,21 @@ public class Interpreter {
     }
   }
 
+  /**
+   * * parseLine
+   * 
+   * This takes in a single line of input from an espresso file and decides what
+   * types of line it has, and then runs the line through corresponding methods
+   * depending on the type line.
+   * 
+   * @param line an array of Strings that contain an individual lines tokens
+   * @return void
+   */
   private void parseLine(String[] line) {
     String initialToken = line[0];
-    if(initialToken.equals("read")){
+    if (initialToken.equals("read")) {
       this.getInput(Arrays.copyOfRange(line, 1, line.length));
-    } else if (initialToken.equals("print")){
+    } else if (initialToken.equals("print")) {
       // This is a print statement
       System.out.println("IS AN OUTPUT STATEMENT");
     } else {
@@ -50,23 +78,33 @@ public class Interpreter {
     }
   }
 
-  private void assignValue(Variable val){
-    
+  private void assignValue(Variable val) {
+
   }
 
+  /**
+   * getInput
+   * 
+   * This method gets input from users and stores it in a variable. This method is
+   * typically called as a result of read call in an espresso file
+   * 
+   * @param line an array of strings that contains the appropriate tokens for a
+   *             read call
+   */
   private void getInput(String[] line) {
-    // ! Should check for line length to ensure the line ends after reading the variable
+    // ! Should check for line length to ensure the line ends after reading the
+    // variable
     Scanner reader = new Scanner(System.in);
     char varToBeAssigned = line[0].charAt(0);
     System.out.print("Enter an integer number for variable " + varToBeAssigned + ": ");
     this.variableTable[varToBeAssigned].setValue(reader.nextInt());
   }
 
-  private void printOutput(String output) {
-
+  private void printOutput(String[] line) {
+    System.out.println(calcInfixExpression(line));
   }
 
-  private int calcInfixExpressions(String[] exp){
+  private int calcInfixExpression(String[] exp) {
     return 1;
   }
 }
