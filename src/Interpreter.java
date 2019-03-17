8d9fae25b9364321ab1618c664fedbcdb9a824b6
Mykal Machon <mykalmachon@gmail.com>
2019-03-17 00:47:12 -0700

    Adding More Comments + Restructure
    + Added more comments to the methods in Interpreter.java to better reflect how the class works
    
    + Restructured the class after taking a look at how I am going to parse infix expressions. I've decided to convert the infix to a postfix, and then solve the postfix. I could have used post-order traversal on an expression tree, but seeing as I need more practice with postfix I figured this would be a better option.
    
    - I may also write a expression tree method for this as well, just not initially.

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Interpreter {
   private LinkedList<String> lines;
@@ -70,26 +71,21 @@ public class Interpreter {
     if (initialToken.equals("read")) {
       this.getInput(Arrays.copyOfRange(line, 1, line.length));
     } else if (initialToken.equals("print")) {
-      // This is a print statement
-      System.out.println("IS AN OUTPUT STATEMENT");
+      this.printOutput(line);
     } else {
-      // This is a assignment statement
-      System.out.println("IS AN ASSIGNMENT STATEMENT");
+      this.assignValue(line);
     }
   }
 
-  private void assignValue(Variable val) {
-
-  }
-
   /**
-   * getInput
+   * * getInput
    * 
    * This method gets input from users and stores it in a variable. This method is
    * typically called as a result of read call in an espresso file
    * 
    * @param line an array of strings that contains the appropriate tokens for a
    *             read call
+   * @return void
    */
   private void getInput(String[] line) {
     // ! Should check for line length to ensure the line ends after reading the
@@ -100,11 +96,84 @@ public class Interpreter {
     this.variableTable[varToBeAssigned].setValue(reader.nextInt());
   }
 
+  /**
+   * * printOutput
+   * 
+   * This method takes in an infix expression, calculates its value, and prints it
+   * to the console.
+   * 
+   * @param line an array of strings that contains the appropriate tokens for a
+   *             printOutput call. This is only an infix expression
+   * @return void
+   */
   private void printOutput(String[] line) {
     System.out.println(calcInfixExpression(line));
   }
 
+  /**
+   * * assignValue
+   * 
+   * This method takes in assignment statements, calculates the right hand side
+   * (an infix expression) and assigns a value to the variable on the left hand
+   * side (a variable in the variable table)
+   * 
+   * @param line an array of strings that contains the appropriate tokens for an
+   *             assignment call. This inlcudes a variable name, an equals sign,
+   *             and an infix expression
+   * @return void
+   */
+  private void assignValue(String[] line) {
+    char varToBeAssigned = line[0].charAt(0);
+    if (line[1].charAt(0) == '=') {
+      int variableValue = this.calcInfixExpression(Arrays.copyOfRange(line, 2, line.length));
+      this.variableTable[varToBeAssigned].setValue(variableValue);
+    } else {
+      // ! THROW SYNTAX ERROR: IMPROPER ASSIGNMENT
+    }
+  }
+
+  /**
+   * * calcInfixExpression
+   * 
+   * This method takes in an infix expression in the form of an array of Strings
+   * and first converts it into a postfix expression, then solves the postfix
+   * expression, then returns the solved value.
+   * 
+   * @param exp an array of strings that contains an infix expression.
+   * @return int: value of the solved infix expression.
+   */
   private int calcInfixExpression(String[] exp) {
+    String[] postfixTokens = convertInfixToPostfix(exp);
+    int evaluatedPostfix = calcPostfixExpression(postfixTokens);
+    return evaluatedPostfix;
+  }
+
+  /**
+   * * convertInfixToPostfix
+   * 
+   * This method takes an infix expression and converts it to a postfix
+   * expression. This is done using a stack and precedence and associativity laws.
+   * 
+   * @param infixExp an array of strings that contains an infix expression.
+   * @return String[]: set of tokens in a postfix expression
+   */
+  private String[] convertInfixToPostfix(String[] infixExp) {
+    // TODO write the method
+    return new String[5];
+  }
+
+  /**
+   * * calcPostfixExpression
+   * 
+   * This method calculates a given postfix expression and returns the value as an
+   * int.
+   * 
+   * @param exp an array of strings that contains an postfix expression
+   * @return int: value of the solved postfix expression
+   */
+  private int calcPostfixExpression(String[] exp) {
+    // TODO write the method
     return 1;
   }
+
 }
