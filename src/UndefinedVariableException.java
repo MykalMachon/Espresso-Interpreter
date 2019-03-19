ab2f9035aeaa50a0e69fb065f3f9ece531f61f5b
Mykal Machon <mykalmachon@gmail.com>
2019-03-18 22:10:33 -0700

    UndefinedVariableException.java Final Comments

--- src/UndefinedVariableException.java
+++ src/UndefinedVariableException.java
@@ -1,6 +1,15 @@
 public class UndefinedVariableException extends Exception {
+  // Needed to Supress Errors
   private static final long serialVersionUID = 1L;
 
+  /**
+   * * UndefinedVariableException
+   * 
+   * This is the constrcutor for the UndefinedVaraibleException to be used with th
+   * Variable class.
+   * 
+   * @param err a string describing the error and why it happened.
+   */
   public UndefinedVariableException(String err) {
     super(err);
   }
