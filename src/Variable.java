a4a46b55ac86043b3d6a9be0e7c273b8d6db0e35
Mykal Machon <mykalmachon@gmail.com>
2019-03-18 22:10:42 -0700

    Variable.java Final Comments

--- src/Variable.java
+++ src/Variable.java
@@ -2,15 +2,37 @@ public class Variable {
   private int value;
   private boolean initialized;
 
+  /**
+   * * Variable
+   * 
+   * The Constructor for the Variable class. Sets this.initialized to false to
+   * signify the fact that the variable has not yet been initialized with a value
+   */
   public Variable() {
     this.initialized = false;
   }
 
+  /**
+   * * setValue
+   * 
+   * setValue sets a Variable Instance's this.value to equal the passed in value.
+   * 
+   * @param value an integer value to set the variables value to
+   * @return void
+   */
   public void setValue(int value) {
     this.value = value;
     this.initialized = true;
   }
 
+  /**
+   * * getValue
+   * 
+   * getValue returns a integer value that the varaible is defined as. If the
+   * variable is not initialized, it throws a new UndefinedVariableException
+   * 
+   * @return int of the variables value
+   */
   public int getValue() throws UndefinedVariableException {
     if (this.initialized) {
       return this.value;
