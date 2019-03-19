b1a9577ae2d9132cd67a4b87a141a997de30f946
Mykal Machon <mykalmachon@gmail.com>
2019-03-18 22:10:10 -0700

    Espresso.java Final Comments

--- src/Espresso.java
+++ src/Espresso.java
@@ -13,12 +13,15 @@ public class Espresso {
       Interpreter espInterpreter = new Interpreter(espFile);
       espInterpreter.init();
     } catch (ArrayIndexOutOfBoundsException e) {
+      // ? Is thrown when the file has not been passed in
       System.out.println("\nThis is a Interpreter for Espresso!");
       System.out.println("You haven't passed a file in to be interpreted\n");
     } catch (IllegalArgumentException e) {
+      // ? Is thrown when the file is not an .esp file
       System.out.println("\nThis is a Interpreter for Espresso!");
       System.out.println("You haven't passed in an .esp file! Please double check your file and try again!\n");
     } catch (FileNotFoundException e) {
+      // ? Is thrown when the text passed in does not lead to a valid file
       System.out.println("\nThis is a Interpreter for Espresso!");
       System.out.println(
           "The file that you've passed in as an arguement either does not exist or could not be found.\nPlease double check your file and try again\n");
